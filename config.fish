3d7992afc5acc6f817be30b182debb73549d41d8
Tahsin <tahsynx@gmail.com>
2019-05-04 06:09:41 +0600

    Remove symbolic link, add hard link

--- config.fish
+++ config.fish
@@ -1,1 +1,11 @@
-/home/tahsin/.config/fish/config.fish
+# gopath
+set -gx GOPATH $HOME/go
+
+# go and gobin
+set -gx PATH /usr/local/go/bin $GOPATH/bin $PATH
+
+# bazel
+set -gx PATH $HOME/bin $PATH
+
+# $EDITOR
+set -gx EDITOR vim
